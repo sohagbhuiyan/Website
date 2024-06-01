c1114257344bb30182e98b5a780bd56b711f4bd9
sohagbhuiyan <smsohag171@gmail.com>
2024-06-01 22:55:20 +0600

    MyPortfolio

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -5,7 +5,16 @@ import { Link } from "react-scroll";
 import contact from "../../assets/c.jpg";
 import navOpen from "../../assets/n.png";
 import navClose from "../../assets/nc.png";
+import { RxCross2 } from "react-icons/rx";
+import { TbMenu2 } from "react-icons/tb";
 const Navbar = () => {
+  const Links = [
+    { name: "Home", link: "/Home" },
+    { name: "Skills", link: "/Service" },
+    { name: "Project", link: "/About" },
+    { name: "Contact Me", link: "/Contact_me" },
+  ];
+  const [menu, setMenu] = useState(false);
   const CvDownload = () => {
     window.open(
       "https://drive.google.com/file/d/1LLslhA-C1LJe0sSR6GvT0Ld7UOZMtPl7/view?usp=sharing",
@@ -13,160 +22,164 @@ const Navbar = () => {
     );
   };
 
-  const [menu, setMenu] = useState(false);
-  const menuref = useRef();
-  const menuclose = () => {
-    console.log("gdf");
-    setMenu((men) => !men);
-  };
   return (
-    <>
-      {menu ? (
-        <img
-          src={navClose}
-          alt="navClose"
-          className="navClose"
-          onClick={menuclose}
-        />
-      ) : (
-        <img
-          src={navOpen}
-          alt="navOpen"
-          className="navOpen"
-          onClick={menuclose}
-        />
-      )}
+    <div className="nav">
+      <div className="navtitle">
+        <img src={logo} alt="" className="navImg" />
+        <span className="">Welcome to Sohag Bhuiyan </span>
+      </div>
+      <div className="navName" onClick={()=>setMenu(!menu)}>
+      {menu? (<TbMenu2/>) : (<RxCross2/>)}
+      </div>
+    </div>
 
-      <nav className="navbar">
-        <img src={logo} alt="logo" className="logo" />
+    // <div>
+    //   {menu ? (
+    //     <img
+    //       src={navClose}
+    //       alt="navClose"
+    //       className="navClose"
+    //       onClick={menuclose}
+    //     />
+    //   ) : (
+    //     <img
+    //       src={navOpen}
+    //       alt="navOpen"
+    //       className="navOpen"
+    //       onClick={menuclose}
+    //     />
+    //   )}
 
-        <div ref={menuref} className="desktopmenu">
-          <Link
-            activeClass="active"
-            to="intro"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
-            className="desktopMenuListItem"
-          >
-            Home
-          </Link>
-          <Link
-            activeClass="active"
-            to="skills"
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={500}
-            className="desktopMenuListItem"
-          >
-            Skills
-          </Link>
-          <Link
-            activeClass="active"
-            to="project"
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={500}
-            className="desktopMenuListItem"
-          >
-            Projects
-          </Link>
+    //   <nav className="navbar">
+    //     <img src={logo} alt="logo" className="logo" />
 
-          <button onClick={CvDownload} className="desktopMenuListItembtn">
-            CV
-          </button>
-          <Link
-            activeClass="active"
-            to="footer"
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={500}
-            className="desktopMenuListItem"
-          ></Link>
-        </div>
+    //     <div ref={menuref} className="desktopmenu">
+    //       <Link
+    //         activeClass="active"
+    //         to="intro"
+    //         spy={true}
+    //         smooth={true}
+    //         offset={-100}
+    //         duration={500}
+    //         className="desktopMenuListItem"
+    //       >
+    //         Home
+    //       </Link>
+    //       <Link
+    //         activeClass="active"
+    //         to="skills"
+    //         spy={true}
+    //         smooth={true}
+    //         offset={-50}
+    //         duration={500}
+    //         className="desktopMenuListItem"
+    //       >
+    //         Skills
+    //       </Link>
+    //       <Link
+    //         activeClass="active"
+    //         to="project"
+    //         spy={true}
+    //         smooth={true}
+    //         offset={-50}
+    //         duration={500}
+    //         className="desktopMenuListItem"
+    //       >
+    //         Projects
+    //       </Link>
 
-        <Link
-          activeClass="active"
-          to="contact"
-          spy={true}
-          smooth={true}
-          offset={-90}
-          duration={500}
-        >
-          <button className="desktopbtn">
-            <img src={contact} alt="contact" className="dektopImg" />
-            Contact Me
-          </button>
-        </Link>
-      </nav>
+    //       <button onClick={CvDownload} className="desktopMenuListItembtn">
+    //         CV
+    //       </button>
+    //       <Link
+    //         activeClass="active"
+    //         to="footer"
+    //         spy={true}
+    //         smooth={true}
+    //         offset={-50}
+    //         duration={500}
+    //         className="desktopMenuListItem"
+    //       ></Link>
+    //     </div>
 
-      {menu && (
-        <div className="mobilemenu">
-          <Link
-            activeClass="active"
-            to="intro"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
-            className="mobileListItem"
-          >
-            Home
-          </Link>
-          <Link
-            activeClass="active"
-            to="skills"
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={500}
-            className="mobileListItem"
-          >
-            Skills
-          </Link>
-          <Link
-            activeClass="active"
-            to="project"
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={500}
-            className="mobileListItem"
-          >
-            Projects
-          </Link>
+    //     <Link
+    //       activeClass="active"
+    //       to="contact"
+    //       spy={true}
+    //       smooth={true}
+    //       offset={-90}
+    //       duration={500}
+    //     >
+    //       <button className="desktopbtn">
+    //         <img src={contact} alt="contact" className="dektopImg" />
+    //         Contact Me
+    //       </button>
+    //     </Link>
+    //   </nav>
 
-          <Link
-            activeClass="active"
-            to="footer"
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={500}
-            className="mobileListItem"
-          ></Link>
-          <Link
-            activeClass="active"
-            to="contact"
-            spy={true}
-            smooth={true}
-            offset={-90}
-            duration={500}
-            className="mobileListItem"
-          >
-            <img src={contact} alt="contact" className="mobileImg" />
-            Contact Me
-          </Link>
-          <button onClick={CvDownload} className="mobileListItem1">
-            CV
-          </button>
-        </div>
-      )}
-    </>
+    //   {menu && (
+    //     <div className="mobilemenu">
+    //       <Link
+    //         activeClass="active"
+    //         to="intro"
+    //         spy={true}
+    //         smooth={true}
+    //         offset={-100}
+    //         duration={500}
+    //         className="mobileListItem"
+    //       >
+    //         Home
+    //       </Link>
+    //       <Link
+    //         activeClass="active"
+    //         to="skills"
+    //         spy={true}
+    //         smooth={true}
+    //         offset={-50}
+    //         duration={500}
+    //         className="mobileListItem"
+    //       >
+    //         Skills
+    //       </Link>
+    //       <Link
+    //         activeClass="active"
+    //         to="project"
+    //         spy={true}
+    //         smooth={true}
+    //         offset={-50}
+    //         duration={500}
+    //         className="mobileListItem"
+    //       >
+    //         Projects
+    //       </Link>
+
+    //       <Link
+    //         activeClass="active"
+    //         to="footer"
+    //         spy={true}
+    //         smooth={true}
+    //         offset={-50}
+    //         duration={500}
+    //         className="mobileListItem"
+    //       ></Link>
+    //       <Link
+    //         activeClass="active"
+    //         to="contact"
+    //         spy={true}
+    //         smooth={true}
+    //         offset={-90}
+    //         duration={500}
+    //         className="mobileListItem"
+    //       >
+    //         <img src={contact} alt="contact" className="mobileImg" />
+    //         Contact Me
+    //       </Link>
+    //       <button onClick={CvDownload} className="mobileListItem1">
+    //         CV
+    //       </button>
+    //     </div>
+    //   )}
+    // </div>
   );
 };
 
